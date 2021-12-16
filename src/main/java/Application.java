f198988b8cbde96850626325751a80fbe0cf16aa
sun <syoun602@gmail.com>
2021-12-16 23:59:01 +0900

    refactor: delete unnecessary lines in application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,13 +3,6 @@ import controller.MachineController;
 public class Application {
     // TODO 구현 진행
     public static void main(String[] args) {
-//        final List<Table> tables = TableRepository.tables();
-//        OutputView.printTables(tables);
-//
-//        final int tableNumber = InputView.inputTableNumber();
-//
-//        final List<Menu> menus = MenuRepository.menus();
-//        OutputView.printMenus(menus);
         MachineController machineController = MachineController.getInstance();
         run(machineController);
     }
